adc320aa197a7c75cba05b6340c21c24920e187e
Vishal Lomte <vishallomte@gmail.com>
2025-07-30 15:57:58 +0530

    Python practice file

---
+++ My first Code.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+'''a="vishal"
+print(a)'''
+'''------------------------------------'''
+a = float(10.2)
+b = float(20.2)
+c=b*a
+print (int(c))
+
+'''------------------------------------'''
+
+a = int(input("enter a numeric value"))
+b = input("enter another numeric value to perform multiplication")
+
+c = (a)*int(b)
+print(c)
+'''------------------------------------'''
