014a5b946c07074d11843e1131accdc96b61ecea
Vishal Lomte <vishallomte@gmail.com>
2025-07-30 18:02:31 +0530

    save file

---
+++ Practice.py
@@ -0,0 +1,26 @@
+# a=int(input("enter math no."))
+# b=int(input("enter english no."))
+# a=5
+# b=10
+# c=a+b
+# if c==50:
+#     print("pass")
+# else:
+#     print("fail")
+    
+a=int(input("enter math no."))
+b=int(input("enter english no."))
+if a+b==50:
+    print("pass")
+else:
+    print("fail") 
+
+
+
+
+
+# a=int(input("enter first number"))
+# if a % 2==0:
+#     print("Number is even")
+# else:
+#     print("number is odd")
---
+++ covid india.py
@@ -0,0 +1,13 @@
+# import the necessary libraries
+import numpy as np 
+import pandas as pd 
+from datetime import date, timedelta
+
+# Visualisation libraries
+import matplotlib.pyplot as plt
+##%matplotlib inline
+import seaborn as sns
+sns.set()
+
+import warnings
+warnings.filterwarnings('ignore')
